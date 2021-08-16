0141f18351d8b1bf43f97564da03add947cb2b6d
ak2296 <mohamad.gholinia@gmail.com>
2021-08-16 16:51:09 +0200

    redirected

--- main.js
+++ main.js
@@ -59,10 +59,10 @@ $("#next").click(function(){
 
     // AUTHORIZE with Spotify (if needed)
     // *************** REPLACE THESE VALUES! *************************
-    let client_id = 'df6968c34f4b4494ac206c7628d2afc7';
+    let client_id = 'e21b3fc22a1e49c18ac957e87f56b5ce';
     // Use the following site to convert your regular url to the encoded version:
     // https://www.url-encode-decode.com/
-    let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only'; // GitHub Pages URL or whatever your public url to this app is
+    let redirect_uri = 'https%3A%2F%2Fak2296.github.io%2FDiggaren2'; // GitHub Pages URL or whatever your public url to this app is
     // *************** END *************************
 
     const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
